40cd2a68f68bc34288c4e579d168cd4f491764eb
Veng Vannet <vannetvengpp@gmail.com>
2021-02-18 14:23:12 +0700

    initail

---
+++ app/src/main/java/com/vannet/databindingkotlin/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.vannet.databindingkotlin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
